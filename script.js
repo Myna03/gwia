3a2d053dafdf82380942fb53081d053427de69fc
Myna03 <86814471+Myna03@users.noreply.github.com>
2022-06-12 09:19:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+   loop:true,
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+   grabCursor:true,
+   loop:true,
+   autoHeight:true,
+   spaceBetween: 20,
+   breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+   },
+});
